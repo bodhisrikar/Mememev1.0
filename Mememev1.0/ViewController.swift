7a1924249183f1365bb7ffb0bea2ef9e94008e0d
Srikar Thottempudi <srikarthottempudi@srikars-air.home>
2018-12-16 09:09:47 -0500

    Initial Commit

---
+++ Mememev1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mememev1.0
+//
+//  Created by Srikar Thottempudi on 12/16/18.
+//  Copyright © 2018 Srikar Thottempudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
